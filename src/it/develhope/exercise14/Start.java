f3b99ecb61c711ac83cd653bb615bc593e4c5329
Jason Di Bartolo <jasondibartolo98@gmail.com>
2022-12-12 17:31:15 +0100

    Initial Commit

---
+++ src/it/develhope/exercise14/Start.java
@@ -0,0 +1,9 @@
+package it.develhope.exercise14;
+
+public class Start {
+    public static void main(String[] args) {
+        String mySurname = "Di Bartolo";
+        System.out.println(mySurname.contains("ni") ? "Contains" : "Doesn't contains");
+
+    }
+}
